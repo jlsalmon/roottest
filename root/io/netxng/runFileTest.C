3553f8b251edae0bbbba3d7242a83ab1eb34db5c
Justin Salmon <jsalmon@cern.ch>
2013-05-17 15:13:06 +0200

    Add async file open test and reformat to ROOT standards

--- root/io/netxng/runFileTest.C
+++ root/io/netxng/runFileTest.C
@@ -1,69 +1,70 @@
 Int_t runFileTest()
 {
-  gDebug = 0;
+   gDebug = 0;
+   TString file = "root://valtical08.cern.ch//localdisk/xrootd/test/test1.root";
+   TFile *f;
 
-  // Open()
-  TFile *f = TFile::Open( "root://valtical08.cern.ch//localdisk/xrootd/test/test1.root" );
+   // Open()
+   f = TFile::Open(file);
+   std::cout << "IsOpen: " << (f->IsOpen() ? "true" : "false") << std::endl;
+   f->Close();
 
-  // IsOpen()
-  std::cout << "IsOpen: " << ( f->IsOpen() ? "true" : "false" ) << std::endl;
+   // Open() async
+   TFileOpenHandle *handle = TFile::AsyncOpen(file);
+   TFile *f = TFile::Open(handle);
+   std::cout << "IsOpen: " << (f->IsOpen() ? "true" : "false") << std::endl;
 
-  // GetSize()
-  Long64_t size = f->GetSize();
-  std::cout << "File size: " << size << std::endl;
+   // GetSize()
+   Long64_t size = f->GetSize();
+   std::cout << "File size: " << size << std::endl;
 
-  // ReadBuffer(3)
-  char *buf = new char[1024];
-  if( f->ReadBuffer( buf, 0, 1024 ) )
-  {
-    std::cout << "Error reading" << std::endl;
-    return 1;
-  }
-  delete buf;
+   // ReadBuffer(3)
+   char *buf = new char[1024];
+   if (f->ReadBuffer(buf, 0, 1024)) {
+      std::cout << "Error reading" << std::endl;
+      return 1;
+   }
+   delete buf;
 
-  // Seek()
-  f->Seek( 1024, TFile::ERelativeTo::kBeg );
+   // Seek()
+   f->Seek(1024, TFile::ERelativeTo::kBeg);
 
-  // ReadBuffer(2)
-  buf = new char[1024];
-  if( f->ReadBuffer( buf, 1024 ) )
-  {
-    std::cout << "Error reading" << std::endl;
-    return 1;
-  }
-  delete buf;
+   // ReadBuffer(2)
+   buf = new char[1024];
+   if (f->ReadBuffer(buf, 1024)) {
+      std::cout << "Error reading" << std::endl;
+      return 1;
+   }
+   delete buf;
 
-  // ReadBuffers()
-  buf = new char[4096];
-  Long64_t pos[4] = {0, 1024, 2048, 3072};
-  Int_t    len[4] = {1024, 1024, 1024, 1024};
-  Int_t    nbuf   = 4;
+   // ReadBuffers()
+   buf = new char[4096];
+   Long64_t pos[4] = { 0, 1024, 2048, 3072 };
+   Int_t len[4] = { 1024, 1024, 1024, 1024 };
+   Int_t nbuf = 4;
 
-  if( f->ReadBuffers(buf, pos, len, nbuf) )
-  {
-    std::cout << "Error reading" << std::endl;
-    return 1;
-  }
-  delete buf;
+   if (f->ReadBuffers(buf, pos, len, nbuf)) {
+      std::cout << "Error reading" << std::endl;
+      return 1;
+   }
+   delete buf;
 
-  // ReOpen()
-  if( f->ReOpen( "update" ) != 0 )
-  {
-    std::cout << "Error reopening file in write mode" << endl;
-    return 1;
-  }
+   // ReOpen()
+   if (f->ReOpen("update") != 0) {
+      std::cout << "Error reopening file in write mode" << endl;
+      return 1;
+   }
 
-  // WriteBuffer()
-  buf = "write me";
-  f->Seek( 0, TFile::ERelativeTo::kEnd );
+   // WriteBuffer()
+   buf = "write me";
+   f->Seek(0, TFile::ERelativeTo::kEnd);
 
-  if( f->WriteBuffer( buf, 8 ) )
-  {
-    std::cout << "Error writing" << std::endl;
-    return 1;
-  }
+   if (f->WriteBuffer(buf, 8)) {
+      std::cout << "Error writing" << std::endl;
+      return 1;
+   }
 
-  f->ReOpen( "read" );
-  f->Close();
-  return 0;
+   f->ReOpen("read");
+   f->Close();
+   return 0;
 }
